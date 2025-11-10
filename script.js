dcf42737f0f0da8df697954c8f713debbf67efaa
zayennn <zaayeennn@gmail.com>
2025-11-11 00:35:50 +0700

    update

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 const data = [
-    {
-        place:'Switzerland Alps',
-        title:'SAINT',
-        title2:'ANTONIEN',
-        description:'Tucked away in the Switzerland Alps, Saint Antönien offers an idyllic retreat for those seeking tranquility and adventure alike. It\'s a hidden gem for backcountry skiing in winter and boasts lush trails for hiking and mountain biking during the warmer months.',
-        image:'https://assets.codepen.io/3685267/timed-cards-1.jpg'
-    },
-    {
-        place:'Japan Alps',
-        title:'NANGANO',
-        title2:'PREFECTURE',
-        description:'Nagano Prefecture, set within the majestic Japan Alps, is a cultural treasure trove with its historic shrines and temples, particularly the famous Zenkō-ji. The region is also a hotspot for skiing and snowboarding, offering some of the country\'s best powder.',
-        image:'https://assets.codepen.io/3685267/timed-cards-2.jpg'
-    },
-    {
-        place:'Sahara Desert - Morocco',
-        title:'MARRAKECH',
-        title2:'MEROUGA',
-        description:'The journey from the vibrant souks and palaces of Marrakech to the tranquil, starlit sands of Merzouga showcases the diverse splendor of Morocco. Camel treks and desert camps offer an unforgettable immersion into the nomadic way of life.',
-        image:'https://assets.codepen.io/3685267/timed-cards-3.jpg'
-    },
-    {
-        place:'Sierra Nevada - USA',
-        title:'YOSEMITE',
-        title2:'NATIONAL PARAK',
-        description:'Yosemite National Park is a showcase of the American wilderness, revered for its towering granite monoliths, ancient giant sequoias, and thundering waterfalls. The park offers year-round recreational activities, from rock climbing to serene valley walks.',
-        image:'https://assets.codepen.io/3685267/timed-cards-4.jpg'
-    },
-    {
-        place:'Tarifa - Spain',
-        title:'LOS LANCES',
-        title2:'BEACH',
-        description:'Los Lances Beach in Tarifa is a coastal paradise known for its consistent winds, making it a world-renowned spot for kitesurfing and windsurfing. The beach\'s long, sandy shores provide ample space for relaxation and sunbathing, with a vibrant atmosphere of beach bars and cafes.',
-        image:'https://assets.codepen.io/3685267/timed-cards-5.jpg'
-    },
-    {
-        place:'Cappadocia - Turkey',
-        title:'Göreme',
-        title2:'Valley',
-        description:'Göreme Valley in Cappadocia is a historical marvel set against a unique geological backdrop, where centuries of wind and water have sculpted the landscape into whimsical formations. The valley is also famous for its open-air museums, underground cities, and the enchanting experience of hot air ballooning.',
-        image:'https://assets.codepen.io/3685267/timed-cards-6.jpg'
-    },
+  {
+    place: 'Switzerland Alps',
+    title: 'SAINT',
+    title2: 'ANTONIEN',
+    description: 'Tucked away in the Switzerland Alps, Saint Antönien offers an idyllic retreat for those seeking tranquility and adventure alike. It\'s a hidden gem for backcountry skiing in winter and boasts lush trails for hiking and mountain biking during the warmer months.',
+    image: 'https://assets.codepen.io/3685267/timed-cards-1.jpg'
+  },
+  {
+    place: 'Japan Alps',
+    title: 'NANGANO',
+    title2: 'PREFECTURE',
+    description: 'Nagano Prefecture, set within the majestic Japan Alps, is a cultural treasure trove with its historic shrines and temples, particularly the famous Zenkō-ji. The region is also a hotspot for skiing and snowboarding, offering some of the country\'s best powder.',
+    image: 'https://assets.codepen.io/3685267/timed-cards-2.jpg'
+  },
+  {
+    place: 'Sahara Desert - Morocco',
+    title: 'MARRAKECH',
+    title2: 'MEROUGA',
+    description: 'The journey from the vibrant souks and palaces of Marrakech to the tranquil, starlit sands of Merzouga showcases the diverse splendor of Morocco. Camel treks and desert camps offer an unforgettable immersion into the nomadic way of life.',
+    image: 'https://assets.codepen.io/3685267/timed-cards-3.jpg'
+  },
+  {
+    place: 'Sierra Nevada - USA',
+    title: 'YOSEMITE',
+    title2: 'NATIONAL PARAK',
+    description: 'Yosemite National Park is a showcase of the American wilderness, revered for its towering granite monoliths, ancient giant sequoias, and thundering waterfalls. The park offers year-round recreational activities, from rock climbing to serene valley walks.',
+    image: 'https://assets.codepen.io/3685267/timed-cards-4.jpg'
+  },
+  {
+    place: 'Tarifa - Spain',
+    title: 'LOS LANCES',
+    title2: 'BEACH',
+    description: 'Los Lances Beach in Tarifa is a coastal paradise known for its consistent winds, making it a world-renowned spot for kitesurfing and windsurfing. The beach\'s long, sandy shores provide ample space for relaxation and sunbathing, with a vibrant atmosphere of beach bars and cafes.',
+    image: 'https://assets.codepen.io/3685267/timed-cards-5.jpg'
+  },
+  {
+    place: 'Cappadocia - Turkey',
+    title: 'Göreme',
+    title2: 'Valley',
+    description: 'Göreme Valley in Cappadocia is a historical marvel set against a unique geological backdrop, where centuries of wind and water have sculpted the landscape into whimsical formations. The valley is also famous for its open-air museums, underground cities, and the enchanting experience of hot air ballooning.',
+    image: 'https://assets.codepen.io/3685267/timed-cards-6.jpg'
+  },
 ]
 
-const _ = (id)=>document.getElementById(id)
-const cards = data.map((i, index)=>`<div class="card" id="card${index}" style="background-image:url(${i.image})"  ></div>`).join('')
+const _ = (id) => document.getElementById(id)
+const cards = data.map((i, index) => `<div class="card" id="card${index}" style="background-image:url(${i.image})"  ></div>`).join('')
 
 
 
-const cardContents = data.map((i, index)=>`<div class="card-content" id="card-content-${index}">
+const cardContents = data.map((i, index) => `<div class="card-content" id="card-content-${index}">
 <div class="content-start"></div>
 <div class="content-place">${i.place}</div>
 <div class="content-title-1">${i.title}</div>
@@ -57,9 +57,9 @@ const cardContents = data.map((i, index)=>`<div class="card-content" id="card-co
 </div>`).join('')
 
 
-const sildeNumbers = data.map((_, index)=>`<div class="item" id="slide-item-${index}" >${index+1}</div>`).join('')
-_('demo').innerHTML =  cards + cardContents
-_('slide-numbers').innerHTML =  sildeNumbers
+const sildeNumbers = data.map((_, index) => `<div class="item" id="slide-item-${index}" >${index + 1}</div>`).join('')
+_('demo').innerHTML = cards + cardContents
+_('slide-numbers').innerHTML = sildeNumbers
 
 
 const range = (n) =>
